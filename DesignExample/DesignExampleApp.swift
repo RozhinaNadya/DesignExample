fd2b6ee54c3174551f30fbce1ed09fd10486a508
RozhinaNadya <zhuk.09@mail.ru>
2022-07-18 11:09:07 +0200

    Initial Commit

---
+++ DesignExample/DesignExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignExampleApp.swift
+//  DesignExample
+//
+//  Created by Надежда on 2022-07-18.
+//
+
+import SwiftUI
+
+@main
+struct DesignExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

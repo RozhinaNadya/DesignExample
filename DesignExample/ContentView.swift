d26bf8f90f9393cbe8320c22d558f821a508b01f
RozhinaNadya <zhuk.09@mail.ru>
2022-07-18 12:20:53 +0200

    Added images, created tthу first step of design

--- DesignExample/ContentView.swift
+++ DesignExample/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { _ in
+            //позволяет сделать view внутри гибким - дочерние views подстраиваются под размер родительского
+            VStack {
+                Image("nikeLogo")
+                    .resizable()
+                    .frame(width: 70, height: 25)
+                
+                ZStack {
+                    Text("Hey")
+                }
+                
+                HStack(spacing: 15) {
+                    Rectangle()
+                        .fill(Color("ColorOne"))
+                        .frame(height: 1) // так получилась полоса
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .fill(Color("ColorOne"))
+                        .frame(height: 1)
+                } .padding(.horizontal, 30) //отступ только по бокам
+                    .padding(.top, 50)
+                
+                HStack(spacing: 25) {
+                    Button(action: {
+                        //
+                    }) {
+                        Image("facebookLogo")
+                            .resizable()
+                            .renderingMode(.original) //чтобы не воспринимал, как кнопку, и не окрашивал в синий
+                            .frame(width: 50, height: 50)
+                    }
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        Image("appleLogo")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                    }
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        Image("twitterLogo")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                    }
+                } .padding(.top, 30)
+            } .padding(.vertical, 100)
+        } .background(Color.black).edgesIgnoringSafeArea(.all)
+            .preferredColorScheme(.dark)
     }
 }
 
